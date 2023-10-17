c48f095cc109ada464d539cf48b06aa6c42e4020
Vitor Paiola <vitinho.souza1234@gmail.com>
2023-10-17 01:40:18 -0300

    Adicionar arquivos de projeto.

---
+++ EstruturaRepeticaoFor3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++) {
+    string[] vet = Console.ReadLine().Split(' ');
+    double a = double.Parse(vet[0], CultureInfo.InvariantCulture);
+    double b = double.Parse(vet[1], CultureInfo.InvariantCulture);
+    double c = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+    double media = (a * 2.0 + b * 3.0 + c * 5.0) / 10.0;
+
+    Console.WriteLine(media.ToString("F1", CultureInfo.InvariantCulture));
+}
